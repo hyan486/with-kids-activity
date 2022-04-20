b71687bac754e9570df5525f1649a869c7cf5a05
hyan486 <hyan486@naver.com>
2022-04-20 06:32:07 +0000

    first commit

--- src/test/java/com/demo/petstore/PetstoreApplicationTests.java
+++ src/test/java/com/demo/petstore/PetstoreApplicationTests.java
@@ -1,13 +1,13 @@
-package com.demo.petstore;
+// package com.demo.petstore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PetstoreApplicationTests {
+// @SpringBootTest
+// class PetstoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
